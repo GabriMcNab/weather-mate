9451add9c215cd9087b700884b7b717620a1ef89
Gabriele Fazio <Gabriele@f8:46:1c:7f:1d:a3.homenet.telecomitalia.it>
2018-04-05 11:13:50 +0200

    Change to UK only

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     super(props);
     this.state = {
       dailyWeather: [],
-      city:'Varazze',
-      country:'IT'
+      city:'London',
+      country:'UK'
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -83,7 +83,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>5 Days Weather Forecast</h1>
+        <h1>UK Weather Forecast</h1>
         <div>
           <form onSubmit={this.handleSubmit}>
             <h3>Choose city: </h3>
